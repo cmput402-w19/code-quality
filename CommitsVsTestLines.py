a90ca7518018217a7569a6fa35d5e8fe7de92181
Struckdown <bfleysher@gmail.com>
2019-03-30 21:21:18 -0600

    Converetd graphs into double line graph for ease of comparison

--- CommitsVsTestLines.py
+++ CommitsVsTestLines.py
@@ -1,4 +1,4 @@
-# Given a .json file, creates a scatterplot of Test Lines Per Commit
+# Given a .json file, creates a scatterplot of Lines of Code (Test Lines and All Lines) Per Commit
 
 import json
 import matplotlib
@@ -15,10 +15,13 @@ for i in range(len(data["test_lines_per_commit"])):
 
 x = xData
 y = data["test_lines_per_commit"]
+y2 = data["total_lines_per_commit"]
 
 fig, ax = plt.subplots()
-ax.scatter(x, y)
+ax.scatter(x, y, label="Test Lines of Code", s=1)
+ax.scatter(x, y2, label="All Lines of Code", s=1)
+ax.legend()
 
-ax.set(xlabel="Commit #", ylabel="Test Related LOC", title="Test Lines Per Commit")
+ax.set(xlabel="Commit #", ylabel="Lines of Code", title="Change in Lines Per Commit")
 ax.grid()
 plt.show()
--- CommitsVsTotalTestLines.py
+++ CommitsVsTotalTestLines.py
@@ -1,4 +1,4 @@
-# Given a .json file, creates a line graph of Total Test Lines Per Commit
+# Given a .json file, creates a double line graph of total lines of Code (Test and All) at each Commit
 
 import json
 import matplotlib
@@ -11,18 +11,25 @@ fp.close()
 
 xData = []
 yData = []
+yData2 = []
 y = 0
+y2 = 0
 for i in range(len(data["test_lines_per_commit"])):
     xData.append(i+1)
     y = y + data["test_lines_per_commit"][i]
+    y2 = y2 + data["total_lines_per_commit"][i]
     yData.append(y)
+    yData2.append(y2)
 
 x = xData
 y = yData
+y2 = yData2
 
 fig, ax = plt.subplots()
-ax.plot(x, y)
+ax.plot(x, y, label="Total Test Lines of Code")
+ax.plot(x, y2, label="Total Lines of Code")
+ax.legend()
 
-ax.set(xlabel="Commit #", ylabel="Total Test Related LOC", title="Total Test Lines Per Commit")
+ax.set(xlabel="Commit #", ylabel="Total Lines of Code", title="Total Lines of Code At Each Commit")
 ax.grid()
 plt.show()
