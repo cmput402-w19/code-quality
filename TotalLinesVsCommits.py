19f9780eed98256a43bb72a29262bd032df44564
Struckdown <bfleysher@gmail.com>
2019-04-03 16:32:58 -0600

    Script now makes plots for all repos

--- TotalLinesVsCommits.py
+++ TotalLinesVsCommits.py
@@ -4,32 +4,57 @@ import json
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-fp = open("flask.json", "r")
-data = json.load(fp)
-fp.close()
+def makeGraph(f, data, fileType):
+    xData = []
+    yData = []
+    yData2 = []
+    y = 0
+    y2 = 0
+    for i in range(len(data["test_lines_per_commit"])):
+        xData.append(i+1)
+        y = y + data["test_lines_per_commit"][i]
+        y2 = y2 + data["total_lines_per_commit"][i]
+        yData.append(y)
+        yData2.append(y2)
+    
+    x = xData
+    y = yData
+    y2 = yData2
+    
+    fig, ax = plt.subplots()
+    ax.plot(x, y, label="Total Test Lines of Code")
+    ax.plot(x, y2, label="Total Lines of Code")
+    ax.legend()
+    
+    ax.set(xlabel="Commit #", ylabel="Total Lines of Code", title="Total Lines of Code At Each Commit")
+    ax.grid()
+    f = f.split(".json")[0]
+    cwd = os.getcwd
+    os.chdir("../TotalLinesVsCommits")
+    plt.savefig('./{}.png'.format(f))
+    plt.close()
+    os.chdir("../" + fileType)
 
-xData = []
-yData = []
-yData2 = []
-y = 0
-y2 = 0
-for i in range(len(data["test_lines_per_commit"])):
-    xData.append(i+1)
-    y = y + data["test_lines_per_commit"][i]
-    y2 = y2 + data["total_lines_per_commit"][i]
-    yData.append(y)
-    yData2.append(y2)
+fileTypes = ["java"]
 
-x = xData
-y = yData
-y2 = yData2
+os.chdir("results")
+try:
+    os.mkdir("TotalLinesVsCommits")
+except:
+    pass
 
-fig, ax = plt.subplots()
-ax.plot(x, y, label="Total Test Lines of Code")
-ax.plot(x, y2, label="Total Lines of Code")
-ax.legend()
+for i in fileTypes:
+    os.chdir(i)
+    allFiles = os.listdir(os.getcwd())
+    for f in allFiles:
+        fp = open(f, "r")
+        data = json.load(fp)
+        fp.close()
 
-ax.set(xlabel="Commit #", ylabel="Total Lines of Code", title="Total Lines of Code At Each Commit")
-ax.grid()
-plt.show()
+        makeGraph(f, data, i)
+        
+    os.chdir("..")
+
+
